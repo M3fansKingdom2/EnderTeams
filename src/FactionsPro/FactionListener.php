4820356ac0931765a0f776462870e445ed54a065
CaptainDuck <jmdimayuga021@gmail.com>
2016-01-24 07:43:27 +0800

    Replaced The Bugs

--- src/FactionsPro/FactionListener.php
+++ src/FactionsPro/FactionListener.php
@@ -124,7 +124,7 @@ class FactionListener implements Listener {
 	}
 	
 	public function onPlayerJoin(PlayerJoinEvent $event) {
-		if($this->plugin->prefs->get("TeamNametags")) {
+		if($this->plugin->prefs->get("FactionNametags")) {
 			$this->plugin->updateTag($event->getPlayer()->getName());
 		}
 	}
